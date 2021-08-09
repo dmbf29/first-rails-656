860eafab2d487c08256c366d0707914ea3692dd3
Douglas Berkley <douglasmberkley@gmail.com>
2021-08-09 13:50:57 +0900

    removed debugging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,6 @@ class PagesController < ApplicationController
 
   def contact
     @members = ['doug', 'lena', 'noemi', 'sylvain', 'sasha', 'yann', 'trouni']
-    # cookies[:score] = 100
-    raise
     if params[:member].present?
       @members = @members.select do |member|
         member.start_with?(params[:member])
